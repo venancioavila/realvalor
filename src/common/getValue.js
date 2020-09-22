16e8b56c2f4bb4baeb701ca6500bc462061e97d8
Venancio Souza <venanciodesouza@icloud.com>
2020-09-22 09:20:17 -0300

    (fix): fix getValue function

--- src/common/getValue.js
+++ src/common/getValue.js
@@ -1,25 +1,25 @@
-const fetchData = require(`./fetchData`);
-const { url } = require(`../configs.json`);
+const fetchData = require("./fetchData");
+const { url } = require("./configs.json");
 
 const getValue = async (type) => {
   switch (type) {
-    case `dolar`:
+    case "dolar":
       return await fetchData(`${url}`);
-    case `bitcoin`:
+    case "bitcoin":
       return await fetchData(`${url}/bitcoin-hoje/`);
-    case `euro`:
+    case "euro":
       return await fetchData(`${url}/euro-hoje/`);
-    case `ouro`:
+    case "ouro":
       return await fetchData(`${url}/ouro-hoje/`);
-    case `iene`:
+    case "iene":
       return await fetchData(`${url}/iene/`);
-    case `dolar-canadense`:
+    case "dolar-canadense":
       return await fetchData(`${url}/dolar-canadense-hoje/`);
-    case `dolar-australiano`:
+    case "dolar-australiano":
       return await fetchData(`${url}/dolar-australiano-hoje/`);
-    case `franco-suico`:
+    case "franco-suico":
       return await fetchData(`${url}/franco-suico-hoje/`);
-    case `libra`:
+    case "libra":
       return await fetchData(`${url}/libra-hoje/`);
   }
 };
